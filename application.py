857ca7022ec2c891eab8f548b52c6bb503deb9ef
wildchaser1703 <toshalimohapatra1@gmail.com>
2025-05-19 18:15:00 +0530

    add requirements.txt

--- application.py
+++ application.py
@@ -1,47 +1,42 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-from logging import FileHandler,WARNING
+from logging import FileHandler, WARNING
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-application=Flask(__name__,template_folder="templates")
+application = Flask(__name__, template_folder="templates")
 file_handler = FileHandler('errorlog.txt')
 file_handler.setLevel(WARNING)
+application.logger.addHandler(file_handler)  
 
 app = application
-
-## Route for a home page
 
 @app.route('/')
 def index():
     return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            gender = request.form.get('gender'),
-            race_ethnicity = request.form.get('ethnicity'),
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch = request.form.get('lunch'),
-            test_preparation_course = request.form.get('test_preparation_course'),
-            reading_score = float(request.form.get('reading_score') or 0),
-            writing_score = float(request.form.get('writing_score') or 0)
-
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('reading_score') or 0),
+            writing_score=float(request.form.get('writing_score') or 0)
         )
         pred_df = data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
-
+        app.logger.info("DataFrame ready for prediction")
+        
         predict_pipeline = PredictPipeline()
-        print("During Prediction")
+        app.logger.info("PredictPipeline initialized")
+        
         results = predict_pipeline.predict(pred_df)
-        print("After Prediction")
-        return render_template('home.html',results=results[0])
-    
-
-if __name__ == "__main__":
-    app.run()
+        app.logger.info("Prediction complete")
+        
+        return render_template('home.html', results=results[0])
